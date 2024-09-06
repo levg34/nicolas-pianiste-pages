193d69ebd3c705a9891a8a228988fc10ed949a1e
levg34 <levg34@users.noreply.github.com>
2024-09-06 10:36:05 +0000

    change urls

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
         port: 3000,
         proxy: {
             '/api': {
-                target: 'https://test.nicolasdross.fr',
+                target: 'https://api-val.nicolasdross.fr',
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, '')
             }
